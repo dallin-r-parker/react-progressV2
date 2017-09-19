f7ef564c32ab6216688e315f47caccefae8aa893
Dallin Parker <dallin.r.parker@gmail.com>
2017-09-19 15:35:32 -0400

    working webpack for react app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+	return(
+		<div>My App!</div>
+	)
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+	entry: {
+		bundle: './src/index.js'
+	},
+	output: {
+		path: path.join(__dirname, 'bundle'),
+		filename: '[name].js'
+	},
+	module: {
+		rules: [
+			{
+				use: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/
+			}
+		]
+	}
+}
